fbd27f81b80afc46ca8c56a3ff06b708f8c426b9
worapit <drm.wmwork@gmail.com>
2021-12-13 21:21:14 +0700

    complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N,int M){
+	if(N > 0 and M > 0){
+		for(int a = 1;a <= N;a++){
+			for(int i = 1; i <= M;i++){
+			cout << 'O';
+			}
+			cout << "\n";
+		}
+	}else{
+		cout << "Invalid input";
+	}
+}
 
 int main(){
 	
